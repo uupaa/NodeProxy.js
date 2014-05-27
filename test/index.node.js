9da7483d03f8283e4e27411462183614c0160414
obara.takao <obara.takao@dena.jp>
2014-05-27 19:52:45 +0900

    follow new WebModule spec

--- test/index.node.js
+++ test/index.node.js
@@ -1,9 +1,12 @@
 require("../node_modules/uupaa.nodemodule.js/lib/NodeModule.js");
+require("../node_modules/uupaa.help.js/node_modules/uupaa.reflection.js/lib/Reflection.js");
+require("../node_modules/uupaa.console.js/lib/Console.js");
+require("../node_modules/uupaa.help.js/lib/Help.js");
 require("../node_modules/uupaa.valid.js/lib/Valid.js");
 require("../node_modules/uupaa.task.js/lib/Task.js");
 require("../node_modules/uupaa.test.js/lib/Test.js");
 require("../node_modules/uupaa.eventlistener.js/lib/EventListener.js");
-require("../node_modules/uupaa.bytearray.js/lib/ByteArray.js");
+require("../node_modules/uupaa.datatype.js/lib/DataType.js");
 require("../node_modules/uupaa.uri.js/lib/URI.js");
 require("../lib/NodeProxy.js");
 require("../release/NodeProxy.min.js");
